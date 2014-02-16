4d56cc42bfec9df08be1c375b3123a5331525818
Julia Teitelbaum <julialt@gmail.com>
2014-02-16 13:45:07 -0500

    moved mosaic project, made colophon page, made icons

--- content/assets/js/app.js
+++ content/assets/js/app.js
@@ -7,36 +7,7 @@ $(function() {
     $('.images a, a[rel="lightbox"]').fluidbox();
 	});
 
-  /*
-  function updatePagingNav() {
-		minID = 1;
-		maxID = $('.homepage-portfolio-item').length;
-	  nextID = parseInt(this.id) + 1;
-	  prevID = parseInt(this.id) - 1;
-	  prev = $('#previous');
-	  next = $('#next');
-	  if (this.id > minID) {
-	  	prev.prop("href", "#"+prevID);
-	  	prev.removeClass('hidden');
-	  } else {
-	  		prev.addClass('hidden');
-	  }
-	  if (this.id < maxID) {
-	  	next.prop("href", "#"+nextID);
-	  	next.removeClass('hidden');
-	  } else {
-	  	next.addClass('hidden');
-	  }
-  }
-  // waypoints for the homepage
-	$('.homepage-portfolio-item').waypoint(
-		function(direction) {
-			updatePagingNav();
-		},
-		{ offset: 10% }
-	);
-	$('#previous').on('hover', updatePagingNav);
-	*/
+
 
 	// Email obfuscator script 2.1 by Tim Williams, University of Arizona
 	// Random encryption key feature by Andrew Moulden, Site Engineering Ltd
